7fb01180b653675f9db0f521dbbca01a14947a28
Andrei Gherzan <andrei@resin.io>
2015-10-26 15:27:02 +0100

    Initial commit
    Signed-off-by: Andrei Gherzan <andrei@resin.io>

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+    echo "Usage: deploy.sh USER HOST"
+    exit 1
+fi
+
+USER=$1
+HOST=$2
+FILE=fb_ili9340.ko
+
+REAL_USER=$(ssh resinvpn -o Hostname=10.2.0.105 'jq -r .username /mnt/conf/config.json')
+
+if [ "$REAL_USER" != "$USER" ]; then
+    echo "Error: $HOST doesn't seem to match user $USER"
+    exit 1
+fi
+
+echo "Deploying $FILE to $HOST..."
+scp  -o Hostname=$HOST ./$FILE resinvpn:/lib/modules/3.18.5/kernel/drivers/video/fbdev/fbtft
